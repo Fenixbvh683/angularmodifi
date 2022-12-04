982046918340b74fb2aae8ffd829dc576eca5fd8
fenixbvh683 <manidelabk031@gmail.com>
2022-12-04 10:42:34 -0300

    subbiendo modificacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
